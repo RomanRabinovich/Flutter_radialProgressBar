e8fa2eb9fc335c0fa53b23477c2630d539001671
RomanRabinovich <roman.kliakhin@gmail.com>
2022-06-06 17:16:38 +0300

    finished but not clean code

--- lib/example.dart
+++ lib/example.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Example extends StatefulWidget {
@@ -14,18 +16,71 @@ class _ExampleState extends State<Example> {
       child: Container(
         width: 100,
         height: 100,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.red),
+        decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+        child: RadialPercentWidget(
+          child: Text(
+            '72 %',
+            style: TextStyle(color: Colors.white, fontSize: 14),
+          ),
         ),
-        child: CustomPaint(painter: MyPainter()),
       ),
+    );
+  }
+}
+
+class RadialPercentWidget extends StatelessWidget {
+  final Widget child;
+  const RadialPercentWidget({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        CustomPaint(painter: MyPainter()),
+        Padding(
+          padding: const EdgeInsets.all(11.0),
+          child: Center(child: child),
+        ),
+      ],
     );
   }
 }
 
 class MyPainter extends CustomPainter {
   @override
-  void paint(Canvas canvas, Size size) {}
+  final double percent = 0.72;
+  void paint(Canvas canvas, Size size) {
+    final backgroundPaint = Paint();
+    backgroundPaint.color = Colors.black;
+    backgroundPaint.style = PaintingStyle.fill;
+    canvas.drawOval(Offset.zero & size, backgroundPaint);
+
+    final filePaint = Paint();
+    filePaint.color = Colors.yellow;
+    filePaint.style = PaintingStyle.stroke;
+    filePaint.strokeWidth = 5;
+    canvas.drawArc(
+      Offset(5.5, 5.5) & Size(size.width - 11, size.height - 11),
+      pi * 2 * percent - (pi / 2),
+      pi * 2 * (1.0 - percent),
+      false,
+      filePaint,
+    );
+
+    final feelPaint = Paint();
+    feelPaint.color = Colors.green;
+    feelPaint.style = PaintingStyle.stroke;
+    feelPaint.strokeWidth = 5;
+    feelPaint.strokeCap = StrokeCap.round;
+    canvas.drawArc(
+      Offset(5.5, 5.5) & Size(size.width - 11, size.height - 11),
+      -pi / 2,
+      pi * 2 * percent,
+      false,
+      feelPaint,
+    );
+  }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDeligate) {
